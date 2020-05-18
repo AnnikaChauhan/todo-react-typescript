d5731f298e03024aa18b3666912db6254858ffff
Annika Chauhan <annikac0711@gmail.com>
2020-05-18 19:17:06 +0100

    first component added, first type added in a file that makes it available for the whole project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
-const App: React.FC = () => {
+import {ToDoListItem} from './ToDoListItem';
+
+
+const todos: Array<Todo> = [{text: 'Walk Somewhere', complete: true}, {text: 'Code Something', complete: false}]
+
+const App: React.FunctionComponent = () => {
   return(
     <div>
       <h1>To Do App</h1>
+      <ToDoListItem todo={todos[0]} />
     </div>
   );
 }
